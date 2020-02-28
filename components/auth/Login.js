6c43bc9294ab437ada20a8489d263bfbacd9bfa4
billkabanga <tkbillkabanga@gmail.com>
2020-02-28 18:06:56 +0300

    add validation with yup

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,23 +1,50 @@
 import {Form, Button} from 'react-bootstrap';
 import styled from 'styled-components';
+import { Formik } from 'formik';
 import AuthTitle from '../common/AuthTitle';
 import FormContainer from '../common/FormContainer';
 
 const LoginForm = (props) => (
-  <FormContainer>
-    <AuthTitle>Login</AuthTitle>
-    <Form>
-      <Form.Group controlId="username">
-        <Form.Control type="text" name="username" placeholder="Username"/>
-      </Form.Group>
-      <Form.Group controlId="password">
-        <Form.Control type="password" name="password" placeholder="Password"/>
-      </Form.Group>
-      <Button variant="dark btn-block" type="submit">
-        Submit
-      </Button>
-    </Form>
-  </FormContainer>
+  <>
+    <Formik
+      initialValues={{username: '', password: ''}}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        handleSubmit
+      }) => (
+        <FormContainer>
+          <AuthTitle>Login</AuthTitle>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Control
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.username}
+                />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                onBlur={handleBlur} 
+                alue={values.password}/>
+            </Form.Group>
+            <Button variant="dark btn-block" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </FormContainer>
+      )}
+    </Formik>
+  </>
 )
 
 export default LoginForm;
